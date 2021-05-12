fd71079859e1d07def9f716b48748fc0f1dba5b7
Lucas Santiago <lu.santi.oli@gmail.com>
2021-05-12 18:40:53 -0300

    feat: clone constructor added

--- graph.hpp
+++ graph.hpp
@@ -3,49 +3,54 @@
 
 #include <list>
 
-#include "graph/vertex.hpp"
 #include "graph/edge.hpp"
+#include "graph/vertex.hpp"
 
 // A mid-level implementation for a graph struct
 class Graph {
-    public:
-    bool is_directional_graph;
-    std::list<Vertex*> all_vertexes;
-    std::list<Edge*> all_edges;
+ public:
+  bool is_directional_graph;
+  std::list<Vertex *> all_vertexes;
+  std::list<Edge *> all_edges;
 
-    //public:
-        // Constructor
-        Graph(bool is_directional_graph = false);
+  // public:
+  // Constructor
+  Graph(bool is_directional_graph = false);
+  Graph(const Graph &g) {
+    this->is_directional_graph = g.is_directional_graph;
+    this->all_vertexes = g.all_vertexes;
+    this->all_edges = g.all_edges;
+  };  // Clone constructor
 
-        // Inserting new values
-        bool insert_vertex(Vertex *vert);
-        bool insert_edge(Edge *edge);
+  // Inserting new values
+  bool insert_vertex(Vertex *vert);
+  bool insert_edge(Edge *edge);
 
-        // Printing the values
-        void print_all_vertexes();
-        void print_all_edges();
-        void print_all_graph();
+  // Printing the values
+  void print_all_vertexes();
+  void print_all_edges();
+  void print_all_graph();
 
-        // Sort graph 
-        void sort_graph();
+  // Sort graph
+  void sort_graph();
 
-        //removing values from the graph
-        void remove_vertex(Vertex *vert);
-        void remove_edge(Edge *edge);
+  // removing values from the graph
+  void remove_vertex(Vertex *vert);
+  void remove_edge(Edge *edge);
 };
 
 // High level implementation for a graph
 class DirectGraph : protected Graph {
-    public:
-    // Construction of a DirectGraph
-    DirectGraph(Vertex *root = nullptr);
+ public:
+  // Construction of a DirectGraph
+  DirectGraph(Vertex *root = nullptr);
 };
 
 // High level implementation for a graph
 class NonDirectGraph : protected Graph {
-    public:
-    // Construction of a NonDirectGraph
-    NonDirectGraph(Vertex *root = nullptr);
+ public:
+  // Construction of a NonDirectGraph
+  NonDirectGraph(Vertex *root = nullptr);
 };
 
 #endif
